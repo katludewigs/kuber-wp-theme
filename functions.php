28d4c522b8d3fde4eceb7ae77e74e7a583a15052
katludewigs <kat.ludewigs@gmail.com>
2025-09-15 10:16:52 +0930

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php
-function kuber_enqueue_styles() {
-    wp_enqueue_style('kuber-style', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'kuber_enqueue_styles');
+// Load CSS from assets/css/main.css
+add_action('wp_enqueue_scripts', function () {
+  wp_enqueue_style(
+    'kuber-main',
+    get_stylesheet_directory_uri() . '/assets/css/main.css',
+    [],
+    filemtime(get_stylesheet_directory() . '/assets/css/main.css')
+  );
+});
 
-function kuber_theme_setup() {
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-}
-add_action('after_setup_theme', 'kuber_theme_setup');
+// Theme supports + a menu location
+add_action('after_setup_theme', function () {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  register_nav_menus([
+    'primary' => __('Primary Menu', 'kuber'),
+  ]);
+});
